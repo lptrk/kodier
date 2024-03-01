7c5cce735112d37c0832d4d633f5752fa5c5d160
lptrk <lktpriv@gmail.com>
2024-03-01 14:13:58 +0100

    extracted ts file to asets

---
+++ src/assets/test.ts
@@ -0,0 +1,45 @@
+interface Shape {
+  name: string;
+  color: string;
+  area(): number;
+}
+
+class Circle implements Shape {
+  name: string;
+  color: string;
+  radius: number;
+
+  constructor(name: string, color: string, radius: number) {
+    this.name = name;
+    this.color = color;
+    this.radius = radius;
+  }
+
+  area(): number {
+    return Math.PI * this.radius ** 2;
+  }
+}
+
+class Rectangle implements Shape {
+  name: string;
+  color: string;
+  width: number;
+  height: number;
+
+  constructor(name: string, color: string, width: number, height: number) {
+    this.name = name;
+    this.color = color;
+    this.width = width;
+    this.height = height;
+  }
+
+  area(): number {
+    return this.width * this.height;
+  }
+}
+
+const circle = new Circle("Circle", "red", 5);
+const rectangle = new Rectangle("Rectangle", "blue", 10, 8);
+
+console.log(circle.area());
+console.log(rectangle.area());
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,16 @@
 mod lexer;
+use std::fs;
+
 fn main() {
-    const TEST_INPUT: &str = r#"
-    interface Shape {
-        name: string;
-        color: string;
-        area(): number;
-    }
+    let file_path = "src/assets/test.ts";
 
-    class Circle implements Shape {
-        name: string;
-        color: string;
-        radius: number;
-
-        constructor(name: string, color: string, radius: number) {
-            this.name = name;
-            this.color = color;
-            this.radius = radius;
+    let test_input = match fs::read_to_string(file_path) {
+        Ok(content) => content,
+        Err(err) => {
+            eprintln!("Fehler beim Lesen der Datei {}: {}", file_path, err);
+            return;
         }
+    };
 
-        area(): number {
-            return Math.PI * this.radius ** 2;
-        }
-    }
-
-    class Rectangle implements Shape {
-        name: string;
-        color: string;
-        width: number;
-        height: number;
-
-        constructor(name: string, color: string, width: number, height: number) {
-            this.name = name;
-            this.color = color;
-            this.width = width;
-            this.height = height;
-        }
-
-        area(): number {
-            return this.width * this.height;
-        }
-    }
-
-    const circle = new Circle("Circle", "red", 5);
-    const rectangle = new Rectangle("Rectangle", "blue", 10, 8);
-
-    console.log(circle.area());
-    console.log(rectangle.area());
-    "#;
-
-    lexer::print_tokens(&lexer::lex(&TEST_INPUT));
+    lexer::print_tokens(&lexer::lex(&test_input));
 }
